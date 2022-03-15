65f5b6128a71b4a0767f58df246b7a5067172f34
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-03-15 15:29:37 +0800

    cors origins remove wildcard

--- fastapi_tut/__init__.py
+++ fastapi_tut/__init__.py
@@ -38,13 +38,13 @@ def register_fastapi_jwt_auth(app):
 		return entry and entry.is_revoked == True
 
 def register_cors(app):
-	# origins = [
-    # "http://localhost:8080",
-	# ]
+	origins = [
+    "http://localhost",
+	]
 
 	app.add_middleware(
 		CORSMiddleware,
-		allow_origins=['*'],
+		allow_origins=origins,
 		allow_credentials=True,
 		allow_methods=["*"],
 		allow_headers=["*"],
