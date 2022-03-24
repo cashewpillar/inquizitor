701d6c4f467b170881d3de1beaeec50ff8234596
Iionsroar <kiseryota023@gmail.com>
2022-03-24 21:24:33 +0800

    added dir for quiz endpoints

---
+++ fastapi_tut/api/api_v1/endpoints/quiz/quiz.py
@@ -0,0 +1,5 @@
+from .quiz import router
+
+# refer to fastapi boiler 
+# @router.get("/")
+# async def read_quiz_by_name
--- fastapi_tut/crud/crud_quiz/quiz.py
+++ fastapi_tut/crud/crud_quiz/quiz.py
@@ -1,9 +1,14 @@
 from sqlmodel import Session
+from typing import List
 
 from fastapi_tut.crud.base import CRUDBase
 from fastapi_tut.models import Quiz, QuizCreate, QuizUpdate
 
 class CRUDQuiz(CRUDBase[Quiz, QuizCreate, QuizUpdate]):
-	pass
-	
+	# Read methods:
+	# 1. Search by name (teacher)
+	def get_multi_by_name(self, db:Session, *, name: str) -> List[Quiz]:
+		"""Search quiz by name attribute only. (not unique)"""
+		return db.query(Quiz).filter(Quiz.name == name).all()
+
 quiz = CRUDQuiz(Quiz)
