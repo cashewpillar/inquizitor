c6192ff95ac526d9ed4ee41046f8ab851e285d6a
jebjebs <virgiliolopez.jev@gmail.com>
2022-03-25 18:05:22 +0800

    removed non-existing file from import

--- fastapi_tut/api/api_v1/api.py
+++ fastapi_tut/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from fastapi_tut.api.api_v1.endpoints import quiz, login, teacher_quiz_crud
+from fastapi_tut.api.api_v1.endpoints import quiz, login
 
 api_router = APIRouter()
 api_router.include_router(login.router, prefix='/login', tags=['login'])
