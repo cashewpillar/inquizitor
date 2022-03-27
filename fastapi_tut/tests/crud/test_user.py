b1e81e94df2825113645e1aae6343b6d1c101538
ralf-t <tabo.ralfmartin@ue.edu.ph>
2022-03-28 00:14:23 +0800

    pytest test_user fix

--- fastapi_tut/tests/crud/test_user.py
+++ fastapi_tut/tests/crud/test_user.py
@@ -11,20 +11,20 @@ def test_create_user(db: Session) -> None:
 	data = fake_user()
 	user_in = UserCreate(**data)
 	user = crud.user.create(db, obj_in=user_in)
-	assert user.email == data["email"]
+	assert user.username == data["username"]
 	assert hasattr(user, "hashed_password")
 
 def test_authenticate_user(db: Session) -> None:
 	data = fake_user()
 	user_in = UserCreate(**data)
 	user = crud.user.create(db, obj_in=user_in)
-	authenticated_user = crud.user.authenticate(db, email=data["email"], password=data["password"])
+	authenticated_user = crud.user.authenticate(db, username=data["username"], password=data["password"])
 	assert authenticated_user
-	assert data["email"] == authenticated_user.email
+	assert data["username"] == authenticated_user.username
 
 def test_not_authenticate_user(db: Session) -> None:
 	data = fake_user()
-	user = crud.user.authenticate(db, email=data["email"], password=data["password"])
+	user = crud.user.authenticate(db, username=data["username"], password=data["password"])
 	assert user is None
 
 def test_check_if_user_is_superuser(db: Session, user: models.User) -> None:
@@ -41,7 +41,7 @@ def test_check_if_user_is_superuser_normal_user(db: Session, user: models.User) 
 def test_get_user(db: Session, user: models.User) -> None:
 	user_2 = crud.user.get(db, id=user.id)
 	assert user_2
-	assert user.email == user_2.email
+	assert user.username == user_2.username
 	assert jsonable_encoder(user) == jsonable_encoder(user_2)
 
 # DOING rename the model marks of user first
@@ -54,5 +54,5 @@ def test_update_user(db: Session, user: models.User) -> None:
 	crud.user.update(db, db_obj=user, obj_in=user_in_update)
 	user_2 = crud.user.get(db, id=user.id)
 	assert user_2
-	assert user.email == user_2.email
+	assert user.username == user_2.username
 	assert verify_password(new_password, user_2.hashed_password)
