35943d8b21e03e2e05a1290cc0becdc6e7d0fa0d
Iionsroar <yawned.eyes@gmail.com>
2022-07-25 00:10:27 +0800

    add index to pk model

--- inquizitor/db/base_class.py
+++ inquizitor/db/base_class.py
@@ -6,4 +6,4 @@ from sqlmodel import Field, SQLModel
 class PKModel(SQLModel):
     """Base model class that adds a 'primary key' column named ``id``."""
 
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True, index=True)
