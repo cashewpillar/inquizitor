7f72923c5f46fd42731338360f047203531d09eb
Iionsroar <yawned.eyes@gmail.com>
2022-08-03 21:35:38 +0800

    add option to get realistic quizzes for initial data

--- inquizitor/db/init_db.py
+++ inquizitor/db/init_db.py
@@ -1,5 +1,7 @@
+import html
 import logging
 import random
+import requests
 from pprint import pformat
 from sqlmodel import Session
 from sqlalchemy.engine import Engine
@@ -67,29 +69,52 @@ def init_test_students(db: Session):
     return test_students
 
 
-def dummy_quiz(db: Session) -> None:
+def dummy_quiz(db: Session, use_realistic_data: bool = False) -> None:
     NUM_QUESTIONS = 5
     test_students = init_test_students(db)
     first_teacher = crud.user.get_by_email(db, email=settings.FIRST_TEACHER_EMAIL)
 
     for i in range(10):
+        realistic_data = list()
+        if use_realistic_data:
+            category = random.randint(9, 32) # available range of categories, see https://opentdb.com/api_config.php
+            while not realistic_data:
+                realistic_data = requests.get(
+                    f'https://opentdb.com/api.php?amount={NUM_QUESTIONS}&type=multiple&category={category}'
+                ).json()['results']
+
         quiz_in = QuizFactory.stub(
             schema_type="create",
             teacher=first_teacher,
             number_of_questions=NUM_QUESTIONS,
         )
+        quiz_in['name'] = html.unescape(realistic_data[0]['category']) if use_realistic_data else quiz_in['name']
         quiz_in = models.QuizCreate(**quiz_in)
         quiz = crud.quiz.create(db, obj_in=quiz_in)
 
         for i in range(NUM_QUESTIONS):
-            question_in = QuestionFactory.stub(schema_type="create", quiz=quiz)
+            question_in = QuestionFactory.stub(
+                schema_type="create", 
+                quiz=quiz,
+            )
+            question_in['content'] = html.unescape(realistic_data[i]['question']) if use_realistic_data else question_in['content']
             question = crud.quiz_question.create(db, obj_in=question_in)
 
             index_correct = random.randrange(0, 4)
-            for i in range(4):
-                choice_in = ChoiceFactory.stub(schema_type="create", question=question)
-                if i == index_correct:
-                    choice_in["is_correct"] = True
+            for j in range(4):
+                choice_in = ChoiceFactory.stub(
+                    schema_type="create", 
+                    question=question,
+                )
+                if j == index_correct:
+                    choice_in['is_correct'] = True
+                    if use_realistic_data:
+                        choice_in['content'] = realistic_data[i]['correct_answer']
+                else:
+                    if use_realistic_data:
+                        choice_in['content'] = realistic_data[i]['incorrect_answers'].pop()
+                choice_in['content'] = html.unescape(choice_in['content'])
+                # choice_in['content'] # required to call, bcs for some choice_in does not get updated
                 choice = crud.quiz_choice.create(db, obj_in=choice_in)
 
         if quiz.id % 2 == 0:
@@ -120,7 +145,7 @@ def dummy_quiz(db: Session) -> None:
                     answer = crud.quiz_answer.create(db, obj_in=answer_in)
 
 
-def init_db(db: Session, engine: Engine) -> None:
+def init_db(db: Session, engine: Engine, use_realistic_data: bool = False) -> None:
     # Tables should be created with Alembic migrations
     # But if you don't want to use migrations, create
     # the tables un-commenting the next line
@@ -145,7 +170,7 @@ def init_db(db: Session, engine: Engine) -> None:
         superuser = crud.user.create(db, obj_in=user_in)  # noqa: F841
 
     init_users(db)
-    dummy_quiz(db)
+    dummy_quiz(db, use_realistic_data=use_realistic_data)
 
 
 def drop_db(engine: Engine) -> None:
